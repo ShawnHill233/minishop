aa012843e39b490c6723b1945d8b0d1af19773a4
司晓翰 <a361162743@hotmail.com>
2018-01-01 18:18:23 +0800

    change user table

--- db/migrate/20171227155812_create_users.rb
+++ db/migrate/20171227155812_create_users.rb
@@ -1,8 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :username
+      t.string :name
       t.string :email
+      t.string :openid
 
       t.timestamps
     end
