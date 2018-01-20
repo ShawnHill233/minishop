b0bb4ce210cb0a9ad780da4892e344c996cf38b3
司晓翰 <a361162743@hotmail.com>
2018-01-20 23:09:10 +0800

    测试回调

--- app/apis/pay_api.rb
+++ app/apis/pay_api.rb
@@ -1,13 +1,14 @@
 class PayAPI < Grape::API
-
+  content_type :xml, 'application/xml'
   formatter :xml, Proc.new { |object|
-    "#{object.to_xml(:skip_instruct => true, root: 'xml', :dasherize => false)}"
+    "<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>"
   }
   format :xml
 
   namespace :pay do
     desc '接收支付结果通知'
     post :notify do
+      puts "get notify *************************"
       body = Nokogiri::XML(request.body)
       order_number = body.xpath('//out_trade_no').text
       puts "=========================="
@@ -16,6 +17,7 @@ class PayAPI < Grape::API
       order = Order.find_by(number: order_number)
       order.complete_pay!
       order.complete!
+
       status 200
       present return_code: 'SUCCESS', return_msg: 'OK'
     end
