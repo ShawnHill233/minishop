2020ae06f64ffd9bb262e3f2c5d66a253d46de7e
司晓翰 <a361162743@hotmail.com>
2018-01-22 17:38:53 +0800

    fix bug

--- app/apis/entities/cart.rb
+++ app/apis/entities/cart.rb
@@ -1,13 +1,13 @@
 module Entities
   class Cart < Grape::Entity
     expose :items_count do |cart|
-      cart.line_items.count
+      cart.line_items.inject(0){ |sum, item| sum + item.quantity }
     end
     expose :checked_amount do |cart|
       cart.checked_amount
     end
     expose :checked_count do |cart|
-      cart.line_items.checked.count
+      cart.line_items.checked.inject(0){ |sum, item| sum + item.quantity }
     end
     expose :line_items, using: Entities::LineItem
   end
