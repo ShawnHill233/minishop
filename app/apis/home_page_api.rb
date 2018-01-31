c65bfc3a9faf76ad115b8376bf05bb5e2736b303
司晓翰 <a361162743@hotmail.com>
2018-01-31 17:26:47 +0800

    improve

--- app/apis/home_page_api.rb
+++ app/apis/home_page_api.rb
@@ -4,7 +4,7 @@ class HomePageAPI < Grape::API
     desc '获取首页数据'
     get do
       banners = Banner.all
-      tire_brands = Brand.where(name: ["米其林", "德国马牌", "固特异", "倍耐力", "普利司通", "邓禄普", "韩泰", "佳通", "玛吉斯"])
+      tire_brands = Brand.where(name: ["米其林", "德国马牌", "固特异", "倍耐力", "普利司通", "邓禄普", "韩泰", "佳通", "玛吉斯", "欧威森"])
       clear_brands = Brand.where(name: ["美孚", "嘉实多", "壳牌"])
       present OpenStruct.new(banners: banners, tire_brands: tire_brands, clear_brands: clear_brands), with: Entities::HomePage
     end
