6379f4bb79d5a09c3985e54ba36474f3806e33af
司晓翰 <a361162743@hotmail.com>
2018-04-09 10:42:59 +0800

    优化获取图片的尺寸

--- app/apis/entities/product.rb
+++ app/apis/entities/product.rb
@@ -5,8 +5,14 @@ module Entities
     expose :name
     expose :description
     expose :price
-    expose :images do |product|
-      product.images.map(&:full_url)
+    expose :images do |product, options|
+      if options[:image_style] == 'product'
+        product.images.map { |image| image.full_url('product') }
+      elsif options[:image_style] == 'large'
+        product.images.map { |image| image.full_url('large') }
+      else
+        product.images.map { |image| image.full_url }
+      end
     end
     expose :product_properties, using: Entities::ProductProperty
   end
